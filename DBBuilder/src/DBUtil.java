c645dfddcf79c2bc849dfa301ee4ca389ff303e5
geomcmaster <geomcmaster@gmail.com>
2017-07-22 21:37:38 -0500

    Framework for database and table creation. Does not include all table creation statements yet.

--- DBBuilder/src/DBUtil.java
+++ DBBuilder/src/DBUtil.java
@@ -1,3 +1,4 @@
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -5,28 +6,55 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- * Utility class for db operations. Intended to be used from JSONUtil.
+ * Utility class for db operations.
  * 
  * @author Geoff
  *
  */
 public class DBUtil {
-	private final String URL = "jdbc:mysql://localhost/CollegeMatch";
+	private final String URL = "jdbc:mysql://localhost/?useSSL=false";
 	private final String USER;
 	private final String PASSWORD;
+	
+	//TODO write all the create statements
+	private final String SCHOOL_TBL = "";
+	private final String USER_TBL = "CREATE TABLE user (ID INT AUTO_INCREMENT, password INT NOT NULL, SAT_SCORE INT,"
+			+ "ACT_SCORE INT, PRIMARY KEY (ID) ); ";
+	private final String REGION_TBL = "";
+	private final String LOC_TBL = "";
+	private final String SCHOOL_LOC_TBL = "";
+	private final String RESIDENCE_TBL = "";
+	private final String FIELD_TBL = "";
+	private final String GENDER_TBL = "";
+	private final String ETHNIC_TBL = "";
+	private final String FAV_FIELD_TBL = "";
+	private final String FAV_SCHOOL_TBL = "";
+	private final String OFFERS_TBL = "";
+	
+	private final String DROP_TABLES_PROC = "DROP PROCEDURE IF EXISTS createTables";
+	private final String CREATE_TABLES_STORED_PROC = "CREATE PROCEDURE createTables() BEGIN DROP TABLE IF EXISTS "
+			+ "school, user, region, location, school_loc, residence, fieldOfStudy, genderDemographics, "
+			+ "ethnicDemographics, favoriteFieldsOfStudy, favoriteSchools, offers; " + SCHOOL_TBL + USER_TBL + 
+			REGION_TBL + LOC_TBL + SCHOOL_LOC_TBL + RESIDENCE_TBL + FIELD_TBL + GENDER_TBL + ETHNIC_TBL + 
+			FAV_FIELD_TBL + FAV_SCHOOL_TBL + OFFERS_TBL + "END";
+	private Statement stmt;
+	private CallableStatement cstmt;
 	public Connection conn;
 	
 	public DBUtil(String user, String pw) {
 		USER = user;
 		PASSWORD = pw;
+		conn = openConnection();	
 	}
 	
-	public void openConnection() {
+	private Connection openConnection() {
+		Connection c = null;
 		try {
-			conn = DriverManager.getConnection(URL, USER, PASSWORD);
+			c = DriverManager.getConnection(URL, USER, PASSWORD);
 		} catch (SQLException e) {
 			System.out.println(e.toString());
 		}
+		return c;
 	}
 	
 	public void closeResultSet(ResultSet rs) {
@@ -39,7 +67,7 @@ public class DBUtil {
 		}
 	}
 	
-	public void closeStatement(Statement stmt) {
+	public void closeStatement() {
 		if (stmt != null) {
 			try {
 				stmt.close();
@@ -49,13 +77,71 @@ public class DBUtil {
 		}
 	}
 	
-	public void closeConnection(Connection conn) {
+	public void closeConnection() {
 		if (conn != null) {
 			try {
+				closeStatement();
 				conn.close();
 			} catch (SQLException e) {
 				System.out.println(e.toString());
 			}
 		}
+	}
+	
+	/**
+	 * Drops database if it exists then creates database.
+	 */
+	public void createDatabase() {
+		try {
+			stmt = conn.createStatement();
+			stmt.execute("DROP DATABASE IF EXISTS CollegeMatch");
+			stmt.execute("CREATE DATABASE CollegeMatch");
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		}
+	}
+	
+	/**
+	 * Drops tables if they exist then creates all tables.
+	 */
+	public void createTables() {
+		setDB();
+		
+		try {
+			stmt.execute(DROP_TABLES_PROC);
+			stmt.execute(CREATE_TABLES_STORED_PROC);
+			cstmt = conn.prepareCall("{call createTables()}");
+			cstmt.execute();
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		}
+	}
+	
+	/**
+	 * Clears all data from all tables.
+	 */
+	public void clearTables() {
+		setDB();
+		
+		//TODO implement
+	}
+	
+	/**
+	 * Sets the connection's database
+	 */
+	private void setDB() {
+		try {
+			conn.setCatalog("CollegeMatch");
+			stmt = conn.createStatement();	//statement needs to be created AFTER catalog set to respect it
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		}
+	}
+	
+	/**
+	 * Set the connection's database
+	 */
+	public void prepareDB() {
+		setDB();
 	}
 }
--- DBBuilder/src/Main.java
+++ DBBuilder/src/Main.java
@@ -1,9 +1,3 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class Main {
 
 	/**
@@ -12,6 +6,11 @@ public class Main {
 	 *  args[0] API key for dataset
 	 *  args[1] MySQL username
 	 *  args[2] MySQL password
+	 *  args[3] Database options
+	 *  	1: drop db if exists. create db.
+	 *  	2: drop tables if exists. create tables.
+	 *  	3: clear all data from tables.
+	 *  	4: do nothing to existing data.
 	 */
 	public static void main(String[] args) {
 		String apiKey = args[0];
@@ -19,9 +18,23 @@ public class Main {
 		String mysqlpw = args[2];
 
 		DBUtil dbutil = new DBUtil(mysqluser, mysqlpw);
+		
+		switch(Integer.parseInt(args[3])) {
+			case 1: dbutil.createDatabase();	//if creating database, fall through to create tables
+			case 2: dbutil.createTables();
+					break;
+			case 3: dbutil.clearTables();
+					break;
+			case 4: dbutil.prepareDB();
+					break;
+			default: break;
+		}
+		
 		JSONUtil jutil = new JSONUtil(apiKey, dbutil);
 		
 		jutil.processAllRecords();
+		
+		dbutil.closeConnection();
 	}
 
 }
