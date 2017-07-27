1936c41f1730404e42f2109df51d3cee9c9c191f
geomcmaster <geomcmaster@gmail.com>
2017-07-27 17:20:15 -0500

    State map

--- CollegeMatchWebApp/src/main/java/Location.java
+++ CollegeMatchWebApp/src/main/java/Location.java
@@ -15,12 +15,65 @@ public class Location {
 	//create stateMap
 	static {
 		stateMap = new HashMap<Integer, String>();
-		//TODO finish the map
 		stateMap.put(9, "CT");
 		stateMap.put(23, "ME");
 		stateMap.put(25, "MA");
 		stateMap.put(33, "NH");
 		stateMap.put(44, "RI");
+		stateMap.put(50, "VT");
+		stateMap.put(10, "DE");
+		stateMap.put(11, "DC");
+		stateMap.put(24, "MD");
+		stateMap.put(34, "NJ");
+		stateMap.put(36, "NY");
+		stateMap.put(42, "PA");
+		stateMap.put(17, "IL");
+		stateMap.put(18, "IN");
+		stateMap.put(26, "MI");
+		stateMap.put(39, "OH");
+		stateMap.put(55, "WI");
+		stateMap.put(19, "IA");
+		stateMap.put(20, "KS");
+		stateMap.put(27, "MN");
+		stateMap.put(29, "MO");
+		stateMap.put(31, "NE");
+		stateMap.put(38, "ND");
+		stateMap.put(46, "SD");
+		stateMap.put(1, "AL");
+		stateMap.put(5, "AR");
+		stateMap.put(12, "FL");
+		stateMap.put(13, "GA");
+		stateMap.put(21, "KY");
+		stateMap.put(22, "LA");
+		stateMap.put(28, "MS");
+		stateMap.put(37, "NC");
+		stateMap.put(45, "SC");
+		stateMap.put(47, "TN");
+		stateMap.put(51, "VA");
+		stateMap.put(54, "WV");
+		stateMap.put(4, "AZ");
+		stateMap.put(35, "NM");
+		stateMap.put(40, "OK");
+		stateMap.put(48, "TX");
+		stateMap.put(8, "CO");
+		stateMap.put(16, "ID");
+		stateMap.put(30, "MT");
+		stateMap.put(49, "UT");
+		stateMap.put(56, "WY");
+		stateMap.put(2, "AK");
+		stateMap.put(6, "CA");
+		stateMap.put(15, "HI");
+		stateMap.put(32, "NV");
+		stateMap.put(41, "OR");
+		stateMap.put(53, "WA");
+		stateMap.put(60, "AS");
+		stateMap.put(64, "FM");
+		stateMap.put(66, "GU");
+		stateMap.put(68, "MH");
+		stateMap.put(69, "MP");
+		stateMap.put(72, "PR");
+		stateMap.put(70, "PW");
+		stateMap.put(78, "VI");
 	}
 	
 	private int id;
@@ -32,13 +85,12 @@ public class Location {
 	private String region;
 	
 	/**
-	 * Returns the abbreviation for a state
+	 * Returns the abbreviation for this state
 	 * 
-	 * @param stateID The state number as stored in the table
 	 * @return The abbreviation for the state
 	 */
-	public String getStateAbbreviation(Integer stateID) {
-		return stateMap.get(stateID);
+	public String getStateAbbreviation() {
+		return stateMap.get(state);
 	}
 	
 	/**
@@ -88,5 +140,19 @@ public class Location {
 	 */
 	public void setZip(int zip) {
 		this.zip = zip;
+	}
+
+	/**
+	 * @return the region
+	 */
+	public String getRegion() {
+		return region;
+	}
+
+	/**
+	 * @param region the region to set
+	 */
+	public void setRegion(String region) {
+		this.region = region;
 	}
 }
