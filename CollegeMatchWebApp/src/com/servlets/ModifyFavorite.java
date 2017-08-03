48b5a09cde6018939a75a2da80f4f844cd946f79
VersorVerbi <doroapodeus@gmail.com>
2017-08-03 08:43:02 -0500

    Add or delete favorites from search results page

---
+++ CollegeMatchWebApp/src/com/servlets/ModifyFavorite.java
@@ -0,0 +1,51 @@
+package com.servlets;
+
+import java.io.IOException;
+import main.java.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ModifyFavorite extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		getServletContext().getRequestDispatcher("/cookie").include(request,response);
+		String username = (String)request.getAttribute("user");
+		
+		String action = request.getParameter("modifyAction");
+		int schoolId = Integer.parseInt(request.getParameter("modifyId"));
+		String[] lastResults = request.getParameter("comboResults").split("^");
+		String userState = request.getParameter("userState");
+		
+		UserDAO db = new UserDAO();
+		
+		if (action == "add") {
+			db.addFavSchool(username, schoolId);
+		} else if (action == "delete") {
+			db.deleteFavSchool(username, schoolId);
+		}
+		
+		List<FavoriteSchool> userFavs = db.getFavSchools(username);
+		String[] outputResults = new String[userFavs.size()];
+		// data format: id
+		for (int i = 0; i < userFavs.size(); i++) {
+			School curFav = userFavs.get(i).getSchool();
+			int curId = 0; //TODO: fix when possible
+			outputResults[i] = "" + curId;
+		}
+		
+		request.setAttribute("favs", outputResults);
+		request.setAttribute("results", lastResults);
+		request.setAttribute("userState", userState);
+		
+		getServletContext().getRequestDispatcher("/results.jsp").forward(request,response);
+	}
+}
