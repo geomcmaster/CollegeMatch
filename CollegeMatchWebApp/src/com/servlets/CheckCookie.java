bd99f33caa4e635519ecf10a2560fd9780c7ee29
VersorVerbi <doroapodeus@gmail.com>
2017-08-03 22:04:52 -0500

    Might fix SaveUserData null username issue

--- CollegeMatchWebApp/src/com/servlets/CheckCookie.java
+++ CollegeMatchWebApp/src/com/servlets/CheckCookie.java
@@ -10,6 +10,10 @@ import javax.servlet.http.Cookie;
 
 public class CheckCookie extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Cookie[] cookies = request.getCookies();
